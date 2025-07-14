5994be970f3ba228b8aaf520e2a00bac041d33ee
Alla-Nazarenko <umnyj.start@gmail.com>
2025-07-14 20:11:41 +0200

    first commit

--- js/modal.js
+++ js/modal.js
@@ -16,3 +16,24 @@
     refs.modal.classList.toggle("is-visible");
   }
 })();
+
+(() => {
+  const refs = {
+    // Додати атрибут data-mobile-menu-open на кнопку відкриття
+    openModalBtn: document.querySelector("[data-mobile-menu-open]"),
+
+    // Додати атрибут data-mobile-menu-close на кнопку закриття
+    closeModalBtn: document.querySelector("[data-mobile-menu-close]"),
+
+    // Додати атрибут data-mobile-menu-modal на бекдроп модалки
+    modal: document.querySelector("[data-mobile-menu-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
+    refs.modal.classList.toggle("is-visible");
+  }
+})();
